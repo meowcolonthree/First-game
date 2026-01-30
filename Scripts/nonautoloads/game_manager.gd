313012ba71f765999d7446cd3dcb29aa88638946
meowcolonthree <meowmiaumrow3@gmail.com>
2026-01-30 15:37:37 +0000

    new features
    added a next level object that appears when the player collects all coins, touching it sends player to next level, also added a "default level" scene that has everything needed to make a new level without having to edit scripts

--- Scripts/nonautoloads/game_manager.gd
+++ Scripts/nonautoloads/game_manager.gd
@@ -6,6 +6,8 @@ var score = 0
 @onready var end_total: Label = $"End total"
 @onready var coins: Node = $"../Coins"
 @onready var coin_total = coins.get_child_count()
+@onready var next_level: AnimatedSprite2D = $NextLevel
+
 
 func add_point_UI():
 	score += 1
@@ -18,5 +20,6 @@ func add_point_UI():
 func final_score():
 	if score == coin_total:
 		end_total.text = "You collected all " + str(score) + " of the " + str(coin_total) + " coins!\nYou win!"
+		next_level.visible = true
 	else:
 		end_total.text = "You collected " + str(score) + " of the " + str(coin_total) + " coins"
